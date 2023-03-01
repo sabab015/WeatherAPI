a98db3a249b5e188b9352d913290a9ad9b196f24
AL SABAB <sabab015@gmail.com>
2023-03-01 17:34:39 +0600

    complete

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-fetch("http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=898c39b8bffa2ef724afffddc26a366b")
+fetch("https://api.openweathermap.org/data/2.5/weather?q=Dhaka&appid=898c39b8bffa2ef724afffddc26a366b&units=metric")
     .then(res => res.json())
     .then(data => {
-        // const user = data.results[0];
-        // const name = user.name;
-        // const userName = `${name.title} ${name.first} ${name.last}`;
-        // document.getElementById("city-name").innerText = userName;
 
-        const city = data.city;
-        const name = city.name
-        // console.log(name);
-        document.getElementById("city-name").innerText = name;
+        console.log(data);
+        document.getElementById("city-name").innerText = data.name;
+
+        const temp = data.main;
+        const tempData = temp.temp;
+        document.getElementById("temp").innerText = tempData;
+
+        const weather_desc = data.weather[0];
+        const details = weather_desc.main;
+        document.getElementById("lead").innerText = details;
     })
