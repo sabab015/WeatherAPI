a412cc21beb3fabccc94c16080ac914a5fec546b
AL SABAB <sabab015@gmail.com>
2023-03-05 16:07:23 +0600

    complete

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=Dhaka&appid=898c39b8bff
     .then(res => res.json())
     .then(data => {
 
+        //checking DHAKA weather
         console.log(data);
         document.getElementById("city-name").innerText = data.name;
 
         const temp = data.main;
         const tempData = temp.temp;
-        document.getElementById("temp").innerText = tempData;
+        document.getElementById("temp").innerHTML = tempData `<p>&deg;C</p>`;
 
         const weather_desc = data.weather[0];
         const details = weather_desc.main;
