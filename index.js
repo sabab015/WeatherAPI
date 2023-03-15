cc459f24d54f9ea111275c0f7357523ca2659466
AL SABAB <sabab015@gmail.com>
2023-03-15 16:11:53 +0600

    complete

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-fetch("https://api.openweathermap.org/data/2.5/weather?q=Dhaka&appid=898c39b8bffa2ef724afffddc26a366b&units=metric")
-    .then(res => res.json())
-    .then(data => {
+document.getElementById("load-city").addEventListener("click", function () {
+    const searchCity = document.getElementById("search-city").value;
 
-        //checking DHAKA weather
-        console.log(data);
-        document.getElementById("city-name").innerText = data.name;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=898c39b8bffa2ef724afffddc26a366b&units=metric`)
+        .then(res => res.json())
+        .then(data => {
 
-        const temp = data.main;
-        const tempData = temp.temp;
-        document.getElementById("temp").innerHTML = tempData `<p>&deg;C</p>`;
+            document.getElementById("city-name").innerText = data.name;
 
-        const weather_desc = data.weather[0];
-        const details = weather_desc.main;
-        document.getElementById("lead").innerText = details;
-    })
+            const temp = data.main;
+            const tempData = temp.temp;
+            document.getElementById("temp").innerText = tempData;
+
+            const weather_desc = data.weather[0];
+            const details = weather_desc.main;
+            document.getElementById("lead").innerText = details;
+        })
+})
